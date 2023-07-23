be3038633424b6b5cb779b3b5f5055dd4b69067d
Akmal Dzaky H <akmaldzaky33@gmail.com>
2023-07-23 17:54:38 +0700

    checkpoint. belum responsive

--- gen-23.05-tailwind-css/tailwind.config.js
+++ gen-23.05-tailwind-css/tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        coba: "6px",
+      },
+      maxWidth: {
+        inputWidth: "55px",
+      },
+      screens: {
+        mobile: "320px",
+        // => @media (min-width: 640px) { ... }
+
+        tablet: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1280px",
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
   },
   plugins: [],
 };
