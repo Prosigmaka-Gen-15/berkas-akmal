dcb46440ea0a1285852c77e47ac57abd8f9c387c
Akmal Dzaky H <akmaldzaky33@gmail.com>
2023-07-24 17:58:37 +0700

    Sudah menggunakan querySelector untuk pilih gambar

---
+++ gen-23.05-javascript-basic/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      margin: {
+        coba: "6px",
+      },
+      flex: {
+        flexGellery: "0 0 auto",
+      },
+      maxWidth: {
+        inputWidth: "55px",
+      },
+      screens: {
+        mobile: "320px",
+        // => @media (min-width: 640px) { ... }
+
+        tablet: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1280px",
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
+  },
+  plugins: [],
+};
