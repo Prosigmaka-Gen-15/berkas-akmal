a99d10d2115409a9bf0d0b4d383a3b173c529c6a
Akmal Dzaky H <akmaldzaky33@gmail.com>
2023-07-30 15:47:37 +0700

    Update: - merubah router di App.jsx - membuat halaman HomePage terpisah dari App.jsx

--- gen-23.05-react-state/src/main.jsx
+++ gen-23.05-react-state/src/main.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AboutPage from './pages/AboutPage.jsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import AboutPage from './pages/AboutPage.jsx';
+// import HomePage from './pages/HomePage';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 
 // mendaftarkan halaman lain
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: 'about',
-    element: <AboutPage />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <HomePage />,
+//   },
+//   {
+//     path: 'about',
+//     element: <AboutPage />,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    {/* <RouterProvider router={router} /> */}
+    <App />
   </React.StrictMode>,
 );
